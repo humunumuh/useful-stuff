23e2fe94610bb058f327c1b47e0320253ccc7897
Zac <zachery.colbert@gmail.com>
2022-10-26 13:55:06 +1000

    Update cuda-check.py

--- cuda-check.py
+++ cuda-check.py
@@ -1,6 +1,8 @@
+# INSTALL CUDA
 # Install cuda for the venv:
 # https://nvidia.github.io/cuda-python/install.html
 
+# INSTALL PYTORCH
 # Install compatible torch:
 # https://pytorch.org/get-started/locally/
 
@@ -11,7 +13,14 @@ print(torch.cuda.is_available()) #Should be True
 print(torch.cuda.device_count()) #Should be 1
 print(torch.cuda.current_device()) #Should be 0
 
-# Install cudnn (further acceleration)
+# INSTALL CUDNN (provides additional acceleration)
 # https://pypi.org/project/nvidia-cudnn/
 # pip install nvidia-pyindex
 # pip install nvidia-cudnn
+
+# Chech cudnn availability
+# https://pytorch.org/docs/stable/backends.html#torch.backends.cudnn.version
+
+print(torch.backends.cudnn.is_available())
+print(torch.backends.cudnn.enabled)
+print(torch.backends.cudnn.version())
