04f98359c5e7937b28b98b6a9d6f9b7b39726657
Zac <zachery.colbert@gmail.com>
2022-10-26 12:05:07 +1000

    Create cuda-check.py
    Check cuda is working with PyTorch

---
+++ cuda-check.py
@@ -0,0 +1,15 @@
+# Install cuda for the venv:
+# https://nvidia.github.io/cuda-python/install.html
+
+# Install compatible torch:
+# https://pytorch.org/get-started/locally/
+
+import torch
+
+print(torch.version.cuda) #Cuda must be compatible with the torch installation
+
+print(torch.cuda.is_available()) #Should be True
+
+print(torch.cuda.device_count()) #Should be 1
+
+print(torch.cuda.current_device()) #Should be 0
